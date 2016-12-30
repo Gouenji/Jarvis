20619f8d2d7d9c800930b299bd393e0da7d238ec
Mehul Kumar <compcode18@gmail.com>
2016-12-30 11:17:23 +0530

    PRE-REQUISITES

---
+++ shell2.sh
@@ -0,0 +1,61 @@
+sudo apt-get install python-pip python-dev build-essential 
+sudo apt-get install python3-pip python3-dev build-essential
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+sudo pip install pygsr
+sudo pip install pygst
+sudo pip install pyttsx
+sudo pip install SpeechRecognition
+sudo apt-get install python3-matplotlib
+sudo pip install gTTS
+sudo apt-get install espeak
+sudo pip install speech
+sudo apt-get install python-pyaudio python3-pyaudio
+sudo apt-get install libasound-dev
+git clone http://people.csail.mit.edu/hubert/git/pyaudio.git
+cd pyaudio
+sudo python setup.py install
+sudo pip install microphone
+sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+sudo apt-get install python-pyaudio
+sudo apt-get install mpg321
+sudo pip install pypiwin32
+sudo apt-get install apache2
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo mysql_install_db
+sudo /usr/bin/mysql_secure_installation
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo service apache2 restart
+sudo apt-get install perl
+sudo apt-get install python-software-properties
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install ruby1.9.3
+sudo apt-get install sphinxsearch
+sudo apt-get install mercurial python-dev python-numpy python-opengl \
+    libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
+    libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev \
+    libtiff5-dev libx11-6 libx11-dev fluid-soundfont-gm timgm6mb-soundfont \
+    xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic fontconfig fonts-freefont-ttf
+hg clone https://bitbucket.org/pygame/pygame
+cd pygame
+python setup.py build
+sudo python setup.py install
+sudo apt-get install mercurial python3-dev python3-setuptools python3-numpy python3-opengl \
+    libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
+    libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev \
+    libtiff5-dev libx11-6 libx11-dev fluid-soundfont-gm timgm6mb-soundfont \
+    xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic fontconfig fonts-freefont-ttf
+hg clone https://bitbucket.org/pygame/pygame
+cd pygame
+python3 setup.py build
+sudo python3 setup.py install
+python -m pygame.tests
+python -m pygame.examples/aacircle
+python -m pygame.examples/aliens
+python -m pygame.examples/freetype_misc
+python -m pygame.examples/glcube
+python -m pygame.examples/sound
+python -m pygame.examples/stars
+
