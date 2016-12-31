ac6e7035fbf5e8160586a87f7d457debeb642c21
Mehul Kumar <compcode18@gmail.com>
2017-01-01 00:55:52 +0530

    Requirements

--- shell2.sh
+++ shell2.sh
@@ -1,11 +1,31 @@
 sudo apt-get install python-pip python-dev build-essential 
 sudo apt-get install python3-pip python3-dev build-essential
-sudo pip install --upgrade pip 
+sudo pip install --upgrade pip
+sudo pip3 install --upgrade pip
+ 
 sudo pip install --upgrade virtualenv 
 sudo pip install pygsr
 sudo pip install pygst
 sudo pip install pyttsx
 sudo pip install SpeechRecognition
+sudo pip install keyring
+sudo pip3 install keyring
+sudo pip install pydispatch
+sudo pip install cssselect
+sudo pip install lxml
+sudo pip install PyDispatcher
+#pyowm
+sudo apt-get install python-pydispatch
+sudo apt-get install python-termcolor
+sudo apt-get install python-voluptuous
+sudo apt-get install python3-voluptuous
+sudo apt-get install python3-keyring
+sudo apt-get install python-keyring
+sudo pip3 install google_speech
+sudo apt-get install libttspico-utils
+sudo pip install httpclient
+sudo apt-get install sox libsox-fmt-mp3
+sudo pip install -r a.txt
 sudo apt-get install python3-matplotlib
 sudo pip install gTTS
 sudo apt-get install espeak
@@ -21,7 +41,17 @@ sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-de
 sudo apt-get install python-pyaudio
 sudo apt-get install mpg321
 sudo pip install pypiwin32
-
+sudo apt-get install apache2
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo mysql_install_db
+sudo /usr/bin/mysql_secure_installation
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo service apache2 restart
+sudo apt-get install perl
+sudo apt-get install python-software-properties
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install ruby1.9.3
 sudo apt-get install sphinxsearch
 sudo apt-get install mercurial python-dev python-numpy python-opengl \
     libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
@@ -41,5 +71,16 @@ hg clone https://bitbucket.org/pygame/pygame
 cd pygame
 python3 setup.py build
 sudo python3 setup.py install
-
+sudo add-apt-repository ppa:dhuggins/cmusphinx
+sudo apt-get update
+sudo apt-get python-xlib python-simplejson python-gtk2 python-gst0.1 \
+gstreamer0.10-pocketsphinx sphinx-common python-sphinxbase \
+python-pocketsphinx sphinxbase-utils git
+python -m pygame.tests
+python -m pygame.examples/aacircle
+python -m pygame.examples/aliens
+python -m pygame.examples/freetype_misc
+python -m pygame.examples/glcube
+python -m pygame.examples/sound
+python -m pygame.examples/stars
 
